b6effb790268e04b9ecc3c6d18adad1315b20ec3
sildvauresberg <slickunit4265752@gmail.com>
2021-06-16 15:46:50 +0300

    Initial commit

---
+++ src/main/java/ru/gunmarket/vakoom/Main.java
@@ -0,0 +1,10 @@
+package ru.gunmarket.vakoom;
+
+import vakoom.gunmarket.ru.TimeService;
+
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(TimeService.utcNow());
+	}
+}
